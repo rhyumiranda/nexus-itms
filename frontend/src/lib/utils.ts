65349bed0eea5f3cec98e69ab22b1369e304f3ff
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-04-20 21:48:57 +0800

    feat: create a pb reusable

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import PocketBase from "pocketbase";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const pb = new PocketBase('http://127.0.0.1:8090');
+
+
