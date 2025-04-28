a1a8f88ce0f4774e8230dbfa00badbba2c609228
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-04-28 17:42:11 +0800

    fix(auth): not directing to dashboard

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -16,13 +16,17 @@ export function middleware(req: NextRequest) {
   }
   
   // Case 2: User tries to access login/signup pages while already authenticated
-  if (req.nextUrl.pathname.startsWith('/auth/')) {
-    if (isAuthenticated) {
+  if (isAuthenticated) {
+    if (req.nextUrl.pathname.startsWith('/auth/') ||
+        req.nextUrl.pathname === '/' ||
+        req.nextUrl.pathname.startsWith('/signup') ||
+        req.nextUrl.pathname.startsWith('/login')) {
       console.log('Redirecting to dasboard');
       const dashboardUrl = new URL('/dashboard', req.url);
       return NextResponse.redirect(dashboardUrl);
     }
   }
+  
   
   return NextResponse.next();
 }
