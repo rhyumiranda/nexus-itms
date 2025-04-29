dea4be4abc8737c361c56020094dd9c8e809fc03
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-04-30 02:02:45 +0800

    feat(create): create a route for serverside task creation

---
+++ frontend/src/app/api/records/tasks/create/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse, NextRequest } from "next/server";
+import { pb } from "@/lib/utils";
+
+export async function POST( request: NextRequest ) {
+  
+  try {
+    
+  } catch (error) {
+    
+  }
+}
