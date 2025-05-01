4fdb4f7eeb736e6e98d2aea8ac39745d7af8dd78
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-01 15:30:04 +0800

    feat(auth): create a cookie for record id and name

--- frontend/src/app/api/auth/login/route.ts
+++ frontend/src/app/api/auth/login/route.ts
@@ -21,6 +21,30 @@ export async function POST( request: NextRequest ) {
       maxAge: 60 * 60 * 24 * 7,
     });
 
+    response.cookies.set( 'pb_user_id', pb.authStore.record?.id ?? '', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    })
+
+    response.cookies.set('pb_user_fn', (pb.authStore.record?.firstName ?? '').trim(), {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    })
+
+    response.cookies.set( 'pb_user_ln', pb.authStore.record?.lastName ?? '', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    })
+
     return response;
   } catch ( error ) {
     return NextResponse.json(
