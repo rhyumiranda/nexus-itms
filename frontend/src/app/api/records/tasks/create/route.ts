69eca51f567decc8d1c45181eae51ae65d1dea9f
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-01 22:05:51 +0800

    feat(create): simplified and updated the field name

--- frontend/src/app/api/records/tasks/create/route.ts
+++ frontend/src/app/api/records/tasks/create/route.ts
@@ -4,15 +4,15 @@ import { pb } from "@/lib/utils";
 export async function POST( request: NextRequest ) {
   
   try {
-    const { title, description, status, priority, deadline } = await request.json();
+    const { title, description, user, status, priority, deadline } = await request.json();
 
     const createTask = await pb.collection( 'tasks' ).create({
-      "task_title": title,
-      "task_description": description,
-      "task_userId": pb.authStore.record?.id,
-      "task_status": status,
-      "task_priority": priority,
-      "task_deadline": deadline
+      "title": title,
+      "description": description,
+      "user": user,
+      "status": status,
+      "priority": priority,
+      "deadline": deadline
     })
 
     console.log("Task created successfully:", createTask);
