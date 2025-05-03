2febdf501daaccef0e1ae8be87a3a2ed5c0f4a6c
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-04 01:14:08 +0800

    refactor: change http method

--- frontend/src/app/api/records/tasks/read/[id]/route.ts
+++ frontend/src/app/api/records/tasks/read/[id]/route.ts
@@ -1,22 +1,19 @@
 import { NextResponse } from 'next/server';
 import { pb } from '@/lib/utils';
 
-export async function POST({ params }: { params: { id: string } }) {
+export async function GET(
+  { params }: { params: { id: string } }
+) {
   const { id } = params;
 
   try {
     const task = await pb.collection('tasks').getOne(id);
-
     return NextResponse.json({
       success: true,
       task,
     });
-    
   } catch (error) {
     console.error('Error fetching task:', error);
-    return new Response(JSON.stringify({ error: 'Failed to fetch task' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return NextResponse.json({ error: 'Failed to fetch task' }, { status: 500 });
   }
 }
