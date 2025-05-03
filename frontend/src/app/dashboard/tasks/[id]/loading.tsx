d59beb5af66bc149468136e619e1ed7506016229
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-03 20:21:30 +0800

    feat(suspense): create a loading skeleton

--- frontend/src/app/dashboard/tasks/[id]/loading.tsx
+++ frontend/src/app/dashboard/tasks/[id]/loading.tsx
@@ -1,9 +1,14 @@
 import { Skeleton } from "@/components/ui/skeleton"
 
-export default function loading() {
+export default function Loading() {
   return (
-    <div>
-      
+    <div className="container mx-auto py-8 px-4">
+      <div className="max-w-3xl mx-auto">
+        <Skeleton className="w-full h-[40px]"/>
+
+        <Skeleton className="w-full h-[350px] mt-4"/>
+      </div>
     </div>
+
   )
 }
