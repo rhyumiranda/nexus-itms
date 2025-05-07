64a72c6240726e12d0bc59f6de7a41cee80d850b
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-08 05:31:14 +0800

    feat: create a designated route for reminders

---
+++ frontend/src/app/dashboard/reminders/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { TaskReminders } from "@/components/task-reminders"
+import { useEffect, useState } from "react";
+
+interface Task {
+  id: string;
+  title: string;
+  description: string;
+  user: string;
+  status: string;
+  priority: string;
+  deadline: string;
+  created: string;
+  updated: string;
+}
+
+export default function page() {
+  const [task, setTask] = useState<Task[]>([]);
+
+  const fetchTasks = async () => {
+    try{
+      const response = await fetch('/api/records/tasks/read');
+      const data = await response.json();
+      console.log("This show the data.tasks: ", data.tasks);
+
+      if (data.success && Array.isArray(data.tasks)) {
+        setTask(data.tasks);
+      }
+
+    } catch(error) {
+      console.error("Error fetching tasks: ", error)
+    }
+  };
+  
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <TaskReminders tasks={task} refreshTasks={fetchTasks}/>
+  )
+}
