02dbc754e3dcf99b4579384f07581fa0ff944bea
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-08 05:27:46 +0800

    feat: make sidebar state global

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { SiteHeader } from "@/components/site-header"
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
 
 export default function layout({children}: {children: React.ReactNode}) {
   return (
-    <div>
-      {children}
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+            {children}
 
-    </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
