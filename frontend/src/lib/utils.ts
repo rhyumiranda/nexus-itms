8afab7f0b0fbff09e4b290959a4208f0754c38f8
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-08 05:30:58 +0800

    feat: update pb link

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const apiUrl = process.env.NEXT_PUBLIC_PB_API_URL || 'http://localhost:8090';
+const apiUrl = process.env.NEXT_PUBLIC_PB_API_URL || 'http://127.0.0.1:8090';
 
 export const pb = new PocketBase(apiUrl);
 
