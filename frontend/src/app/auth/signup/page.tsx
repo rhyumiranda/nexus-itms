c4594fb2c19da653dbf4f7f66101484997a0e4bd
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-17 03:24:53 +0800

    feat: http method

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -1,25 +1,38 @@
-import Image from "next/image"
+import Image from "next/image";
 
-import { LoginForm } from "@/components/signup-form"
+import { SignUpForm } from "@/components/signup-form";
 
 export default function SignUpPage() {
   return (
-    <div className="bg-secondary flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <Image 
-              src="/logo/nexus_logo_white.svg" 
-              alt="Nexus Logo" 
-              width={16} 
-              height={16}
-              className="size-4"
-            />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <Image
+                src="/logo/nexus_logo_white.svg"
+                alt="Nexus Logo"
+                width={16}
+                height={16}
+                className="size-4"
+              />
+            </div>
+            nexus.sphere
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignUpForm />
           </div>
-          Nexus
-        </a>
-        <LoginForm />
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/images/purplegradient.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.7]"
+        />
       </div>
     </div>
-  )
+  );
 }
