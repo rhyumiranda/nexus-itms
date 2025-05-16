dfdc4568c42fbed51c71d2e8e21a94593034996b
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-17 03:40:00 +0800

    chore: clean up

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -58,7 +58,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={data.navMain} />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser/>
       </SidebarFooter>
     </Sidebar>
   )
