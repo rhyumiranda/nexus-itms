4d231bb53912ab1a99131ded18b60503d91dc1c1
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-17 22:08:05 +0800

    fix: link routes

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -7,7 +7,7 @@ export default function SignUpPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <a href="/" className="flex items-center gap-2 font-medium">
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <Image
                 src="/logo/nexus_logo_white.svg"
@@ -17,7 +17,7 @@ export default function SignUpPage() {
                 className="size-4"
               />
             </div>
-            nexus.sphere
+            nexu.sphere
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
